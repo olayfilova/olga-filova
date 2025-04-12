ac49afcd327b9462fee1359b8c0ea2ff63dc2b5f
olafilova <ofilova@gmail.com>
2025-04-12 12:28:59 +0100

    les-1 sys.getsizeof()

---
+++ o_n.py
@@ -0,0 +1,21 @@
+# for i in [1, 2, 3, 4, ]:
+#     if i == 1:
+#         print(i)
+
+list_a = [1, 2, 3]
+for i in list_a:
+    for j in list_a:
+        # print(i, j)
+        if j == 1:
+            print(i)
+
+# ### migration circle
+# li=[1,'jj',]
+# x=li.append("aaa")
+# print(li)
+#
+#
+# for i in range(4,100):
+#     li.append(i*10)
+#     print(f"address_index_0={id(li[0])}")
+#     #print(li)

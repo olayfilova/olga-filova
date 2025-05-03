082791584f50ca67c14d30cc54d438baa64d206c
olafilova <ofilova@gmail.com>
2025-05-03 20:53:01 +0100

    initial commit

--- first_app/models/__init__.py
+++ first_app/models/__init__.py
@@ -1,13 +1,2 @@
 from .student import Student
 from .course import Course
-from .department import Department
-from .employee import Employee
-from .position import Position
-from .company import Company
-from .experiment import Experiment, Article
-from .monthly_salary import MonthlySalary
-from .leave import Leave
-
-from django.db import models
-
-
--- first_app/models/course.py
+++ first_app/models/course.py
@@ -4,9 +4,3 @@ from django.db import models
 class Course(models.Model):
     name=models.CharField(max_length=100)
     description =models.TextField(max_length=1000)
-    learning_plan=models.TextField(blank=True, null=True)
-
-
-
-
-

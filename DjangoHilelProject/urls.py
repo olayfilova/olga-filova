d905cfe2484821d00595b2d5b05ac22a914066d0
olafilova <ofilova@gmail.com>
2025-05-05 19:42:48 +0100

    commit

--- DjangoHilelProject/urls.py
+++ DjangoHilelProject/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from first_app import views
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('first_app.urls')),

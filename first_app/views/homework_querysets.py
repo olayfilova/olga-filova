a8a87d6a0fd11593e8bab44a9ed8763070721c2d
olafilova <ofilova@gmail.com>
2025-05-12 20:04:40 +0100

    fixed homeworkqueryset links to display possitions name & status

--- first_app/views/homework_querysets.py
+++ first_app/views/homework_querysets.py
@@ -1,3 +1,4 @@
+from django.db.models.functions import Lower
 from django.shortcuts import render
 from django.db.models import Q
 from django.http import HttpResponse
@@ -14,7 +15,7 @@ def homework_querysets(request):
 
     dept_names_with_managers=Department.objects.filter(positions__is_manager=True).values('name').distinct()
 
-    sorted_positions=Position.objects.order_by('title').values('title', 'is_active')
+    sorted_positions=Position.objects.order_by(Lower('title'), 'title').values('title', 'is_active')
 
     context={
         'departments_with_managers': departments_with_managers,

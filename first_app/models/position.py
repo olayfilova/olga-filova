9f56497639326362fa687a6b2ef8d83f85d34f5b
olafilova <ofilova@gmail.com>
2025-06-20 23:28:05 +0100

    12 - merge after z correction

--- first_app/models/position.py
+++ first_app/models/position.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Position(models.Model):
-    title=models.CharField(max_length=50)
-    is_manager=models.BooleanField(default=False)
-    is_active=models.BooleanField(default=True)
-    department=models.ForeignKey('Department', on_delete=models.CASCADE)
-    monthly_rate=models.IntegerField(default=0)
-    # Removing the problematic salary field
+    title = models.CharField(verbose_name=_("Title"), max_length=200)
+    is_manager = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    department = models.ForeignKey("Department", on_delete=models.CASCADE)
+    description = models.CharField(verbose_name=_("Job Description"), max_length=500, default="")
+    monthly_rate = models.IntegerField(default=0, verbose_name=_("Monthly Rate"))
+
 
     def __str__(self):
-        return f'{self.title}({self.department})'
-
+        return f"{self.title} ({self.department})"
---
+++ first_app/translation.py
@@ -0,0 +1,8 @@
+from modeltranslation.translator import TranslationOptions, register
+
+from first_app.models import Position
+
+
+@register(Position)
+class PositionTranslationOption(TranslationOptions):
+    fields = ("title", "description")

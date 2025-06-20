c9367528e63cdbac0032ae203daf652770b8da23
olafilova <ofilova@gmail.com>
2025-06-20 01:21:28 +0100

    12 - merge after z correction

--- DjangoHillelProject/settings.py
+++ DjangoHillelProject/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoHilelProject.urls'
+ROOT_URLCONF = 'DjangoHillelProject.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoHilelProject.wsgi.application'
+WSGI_APPLICATION = 'DjangoHillelProject.wsgi.application'
 
 
 # Database

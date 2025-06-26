ab051c917de00811feaa27c10851d571f2251d50
olafilova <ofilova@gmail.com>
2025-06-26 21:05:53 +0100

    django rest framework

--- DjangoHillelProject/urls.py
+++ DjangoHillelProject/urls.py
@@ -20,6 +20,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from general.views import HomePageView
+from general.urls import urlpatterns as general_urls
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
